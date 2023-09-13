a5846d677bbf6d50dfec7bebc46db2227d1b7648
Chu9woww <chu9woww@163.com>
2023-09-13 23:17:21 +0800

    Site updated: 2023-09-13 23:17:19

---
+++ 2131.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<iostream>
+#include<cstdio>
+using namespace std;
+typedef struct Node{
+	int Data[1005];
+	int t;
+} mystack;
+void change(int num, int R){
+	if(num == 0){
+		cout << 0;	
+		return;	
+	}
+	int s = num, y = 0;
+	mystack *head = (mystack *)malloc(sizeof(mystack));
+	head->t = -1;
+	while(s){
+		y = s%R;
+		s = s/R;
+		head->t++;
+		head->Data[head->t] = y;
+	}
+	for(int i = head->t; i >= 0; i--)
+		cout << head->Data[i];
+}
+int main(){
+	int num, R;
+	cin >> num >> R;
+	change(num, R);
+	system("pause");
+	return 0;
+}
